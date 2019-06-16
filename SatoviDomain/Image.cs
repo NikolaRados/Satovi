ea8d4fd9c73fd0a4cc3790b16703b840c5e060e0
NikolaRados <51878414+NikolaRados@users.noreply.github.com>
2019-06-16 16:43:29 +0200

    Add files via upload

---
+++ SatoviDomain/Image.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SatoviDomain
+{
+    public class Image : BaseEntity
+    {
+        public string Src { get; set; }
+
+        public string Alt { get; set; }
+
+        public string Title { get; set; }
+
+        public Product Product { get; set; }
+    }
+}
